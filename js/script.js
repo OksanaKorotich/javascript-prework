e51098c5fd7286213dbe1e9c2905a4d543b99a71
Oksana Korotich <o.korotich@gmail.com>
2022-11-06 19:24:53 +0100

    Add game result

--- js/script.js
+++ js/script.js
@@ -34,5 +34,43 @@ else if(playerInput == '2'){
 else if (playerInput == '3'){
     playerMove = 'nożyce';
 }
+else if (playerInput < '1'){
+    playerMove = 'nieznany ruch';
+}
+
+else if (playerInput > '3'){
+    playerMove = 'nieznany ruch';
+}
+
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Wygram!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Wygram!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Wygram!');
+}
+else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+}
